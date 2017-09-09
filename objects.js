461767deea602dd3237e7346423bc5e8cb0a0249
Danny Avery <danny.w.avery@gmail.com>
2017-09-09 15:40:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(obj, key, value) {
   return Object.assign({}, obj, { [key]: value })
 }
 
-function removeFromPlaylist(obj, key) {
-  return delete obj.key
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName
 }
